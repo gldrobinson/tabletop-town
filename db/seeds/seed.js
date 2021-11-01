9c94f2fa53db2f4a1eae789584550e54d506efeb
gldrobinson <gldrobinson@hotmail.co.uk>
2021-11-01 14:52:57 +0000

    Completes seed function to add data into the database

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,33 +1,37 @@
 const db = require("../connection");
 const format = require("pg-format");
+const {
+  formatCategoryData,
+  formatUserData,
+  formatCommentData,
+  formatReviewData,
+} = require("../../utils/formatData.utils");
 
 const seed = async (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
   // 1. Drop tables if exists... comments, reviews, user, category
-  console.log("Dropping comments table");
   await db.query(`DROP TABLE IF EXISTS comments;`);
-  console.log("Dropping comments reviews");
   await db.query(`DROP TABLE IF EXISTS reviews;`);
-  console.log("Dropping comments users");
   await db.query(`DROP TABLE IF EXISTS users;`);
-  console.log("Dropping comments categories");
   await db.query(`DROP TABLE IF EXISTS categories;`);
 
   // 2. create tables ... category, user, reviews, comments
-  console.log(`Adding categories table`);
+  // categories table
   let query = `CREATE TABLE categories (
     slug VARCHAR(40) PRIMARY KEY NOT NULL,
     description TEXT
   );`;
   await db.query(query);
-  console.log(`Adding users table`);
+
+  // users table
   query = `CREATE TABLE users (
     username VARCHAR(40) PRIMARY KEY NOT NULL,
     avatar_url TEXT,
     name VARCHAR(80)
   );`;
   await db.query(query);
-  console.log(`Adding reviews table`);
+
+  // reviews table
   query = `CREATE TABLE reviews (
     review_id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
@@ -40,7 +44,7 @@ const seed = async (data) => {
     review_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
   );`;
   await db.query(query);
-  console.log(`Adding reviews comments`);
+  // comments table
   query = `CREATE TABLE comments (
     comment_id SERIAL PRIMARY KEY,
     author VARCHAR(40) REFERENCES users(username) NOT NULL,
@@ -50,10 +54,35 @@ const seed = async (data) => {
     comment_body TEXT
   );`;
   await db.query(query);
-  // 3. insert data
-  console.log(`Inserting categories data`);
-  // insert... values %L,
-  // array of vals [[..], [...]...]
+
+  // 3. insert data into tables
+  // categories table
+  query = `INSERT INTO categories 
+  (slug, description)
+  VALUES
+  %L;`;
+  await db.query(format(query, formatCategoryData(categoryData)));
+
+  // users table
+  query = `INSERT INTO users
+  (username, avatar_url, name)
+  VALUES 
+  %L;`;
+  await db.query(format(query, formatUserData(userData)));
+
+  //reviews table
+  query = `INSERT INTO reviews
+  (title, review_body, designer, review_image_url, votes, category, owner, review_created_at)
+  VALUES 
+  %L;`;
+  await db.query(format(query, formatReviewData(reviewData)));
+
+  // comments table
+  query = `INSERT INTO comments
+  (author, review_id, votes, comment_created_at, comment_body)
+  VALUES 
+  %L;`;
+  await db.query(format(query, formatCommentData(commentData)));
 };
 
 module.exports = seed;
