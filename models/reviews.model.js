0982bce0ccb85b2644372e914ab5a7453bb7be5a
gldrobinson <gldrobinson@hotmail.co.uk>
2021-11-15 17:59:30 +0000

    Changes default in model for testing

--- models/reviews.model.js
+++ models/reviews.model.js
@@ -113,6 +113,6 @@ exports.selectReviewsDebug = (sort_by, order) => {
     order = "desc";
   }
 
-  const query = `SELECT * FROM reviews ORDER BY created_at desc;`;
+  const query = `SELECT * FROM reviews ORDER BY title desc;`;
   return db.query(query).then(({ rows }) => rows);
 };
